bfba6729b3a141ff2033478f5a4fd438f325b88c
Junior <junior.jsrc96@gmail.com>
2024-03-16 14:56:20 -0300

    Remover placeholders

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,23 +17,21 @@ const LoginPage = () => {
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <div className="relative">
                   <input
-                    autoComplete="off"
+                    autoComplete="off" // Desactiva el autocompletado
                     id="email"
                     name="email"
                     type="text"
                     className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
-                    placeholder="email"
                   />
                   <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email</label>
                 </div>
                 <div className="relative">
                   <input
-                    autoComplete="off"
+                    autoComplete="off" // Desactiva el autocompletado
                     id="password"
                     name="password"
                     type="password"
                     className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
-                    placeholder="contraseña"
                   />
                   <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Contraseña</label>
                 </div>
