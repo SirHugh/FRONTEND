9ed7b6f0c81a816f1c2f4c6a3d45accb59749a82
Junior <junior.jsrc96@gmail.com>
2024-03-16 14:50:39 -0300

    Login reparado

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,23 +2,7 @@ import React, { useState, useContext } from 'react';
 import AuthContext from "../context/AuthContext";
 
 const LoginPage = () => {
-  const { loginUser } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      await loginUser({ email, password });
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  const closeModal = () => {
-    setError(null);
-  };
+  let { loginUser } = useContext(AuthContext);
 
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
@@ -29,7 +13,7 @@ const LoginPage = () => {
             <div>
               <h1 className="text-2xl font-semibold">Introduzca sus credenciales</h1>
             </div>
-            <form onSubmit={handleLogin} className="divide-y divide-gray-200">
+            <form onSubmit={loginUser} className="divide-y divide-gray-200">
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <div className="relative">
                   <input
@@ -39,8 +23,6 @@ const LoginPage = () => {
                     type="text"
                     className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
                     placeholder="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
                   />
                   <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email</label>
                 </div>
@@ -52,8 +34,6 @@ const LoginPage = () => {
                     type="password"
                     className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
                     placeholder="contraseña"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
                   />
                   <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Contraseña</label>
                 </div>
@@ -65,14 +45,6 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-      {error && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
-          <div className="bg-white p-4 rounded shadow-md">
-            <p className="text-red-600">{error}</p>
-            <button onClick={closeModal} className="mt-2 bg-blue-500 text-white px-4 py-2 rounded">Cerrar</button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
