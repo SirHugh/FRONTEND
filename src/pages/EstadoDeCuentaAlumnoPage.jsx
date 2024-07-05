6091bcb991d04774169a32f98f3a570cb3acd781
JuniorRojas <junior.jsrc96@fpuna.edu.py>
2024-07-04 21:56:48 -0400

    Remover filtrado innecesario. getArancel no trae los aranceles de la matricula

--- src/pages/EstadoDeCuentaAlumnoPage.jsx
+++ src/pages/EstadoDeCuentaAlumnoPage.jsx
@@ -26,19 +26,17 @@ const EstadoDeCuentaAlumnoPage = ({ idAlumno }) => {
         const alumnoData = alumnoRes.data;
         setAlumno(alumnoData);
 
-        console.log("Datos guardados en alumno: " + JSON.stringify(alumnoData));
-
         // 3. Obtener las matrículas del alumno para el período activo
         const matriculaRes = await searchMatricula(true, alumnoData.cedula, "");
         const matriculaAlumno = matriculaRes.data; // Suponiendo que el primer resultado es la matrícula activa del alumno
         setMatricula(matriculaAlumno);
 
-        console.log("Datos de la matrícula: " + JSON.stringify(matriculaAlumno));
-
         // 4. Obtener los aranceles asociados a la matrícula
         if (matriculaAlumno) {
           const arancelRes = await getArancel(true, matriculaAlumno.id_matricula);
-          setAranceles(arancelRes.data);
+          const arancelAlumno = arancelRes.data;
+          setAranceles(arancelAlumno);
+          
         } else {
           toast.error("No se encontraron matrículas para el alumno.");
         }
@@ -47,9 +45,20 @@ const EstadoDeCuentaAlumnoPage = ({ idAlumno }) => {
       }
     };
 
+    console.log("Aranceles " + JSON.stringify(aranceles));
     fetchPeriodoMatriculaAranceles();
   }, [id]);
 
+  // Extraer la cédula de los aranceles y filtrar
+  const filteredAranceles = aranceles.filter(arancel => {
+    if (arancel.alumno) {
+      const [arancelCedula] = arancel.alumno.split("-");
+      return arancelCedula === alumno.cedula;
+    }
+    return false;
+  });
+
+  console.log("Datos filtrados de aranceles: "+JSON.stringify(filteredAranceles))
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Estado de Cuenta del Alumno</h1>
@@ -73,16 +82,22 @@ const EstadoDeCuentaAlumnoPage = ({ idAlumno }) => {
           <Table.HeadCell>Estado</Table.HeadCell>
         </Table.Head>
         <Table.Body>
-          {aranceles.map((arancel, index) => (
-            <Table.Row key={index}>
-              <Table.Cell>{arancel.id_producto}</Table.Cell>
-              <Table.Cell>{CurrencyFormatter(arancel.monto)}</Table.Cell>
-              <Table.Cell>{Months[new Date(arancel.fecha_vencimiento).getMonth()].name}</Table.Cell>
-              <Table.Cell>{arancel.nro_cuota}</Table.Cell>
-              <Table.Cell>{DateFormatter(new Date(arancel.fecha_vencimiento))}</Table.Cell>
-              <Table.Cell>{arancel.es_activo ? "Pendiente" : "Pagado"}</Table.Cell>
+          {aranceles.length > 0 ? (
+            aranceles.map((arancel, index) => (
+              <Table.Row key={index}>
+                <Table.Cell>{arancel.id_producto}</Table.Cell>
+                <Table.Cell>{CurrencyFormatter(arancel.monto)}</Table.Cell>
+                <Table.Cell>{Months[new Date(arancel.fecha_vencimiento).getMonth()].name}</Table.Cell>
+                <Table.Cell>{arancel.nro_cuota}</Table.Cell>
+                <Table.Cell>{DateFormatter(new Date(arancel.fecha_vencimiento))}</Table.Cell>
+                <Table.Cell>{arancel.es_activo ? "Pendiente" : "Pagado"}</Table.Cell>
+              </Table.Row>
+            ))
+          ) : (
+            <Table.Row>
+              <Table.Cell colSpan="6">No se encontraron aranceles para este alumno.</Table.Cell>
             </Table.Row>
-          ))}
+          )}
         </Table.Body>
       </Table>
     </div>
